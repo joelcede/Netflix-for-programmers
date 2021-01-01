e92d7ae199afb72141599c3088969a14f6503310
[AK-47-SOIJ] <jioska_one@outlook.es>
2021-01-01 13:52:19 -0500

    add nav html v1.0

--- clon_netflix/Netflix_Clon/settings.py
+++ clon_netflix/Netflix_Clon/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'Netflix_Clon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +128,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = '/media/'
