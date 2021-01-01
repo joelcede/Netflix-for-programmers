ec68da2c7edba0c06afe94a1c3da0441c123c234
[AK-47-SOIJ] <jioska_one@outlook.es>
2020-12-31 20:37:36 -0500

    agg model from db and modifieds

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Netflix-for-programmers
+# Netflix for programmers
 
 ##### Hi, this is a netflix clone but with educational videos.
 
@@ -7,12 +7,12 @@
 ![](https://i.imgur.com/BzgB1T3.jpg) `My first project without tutorials :)`
 
 ## Requirements
-| technologies  | version |
+| Technologies  | Version |
 | ------------- | ------------- |
 | Python  | 3.7  |
 | Docker | 20.10  |
 
-## local installation
+## Local installation
 - #### `docker-compose build`
 
 - #### `docker-compose up`
--- clon_netflix/users/models.py
+++ clon_netflix/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    """user model"""
+
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+
+    country = models.CharField(max_length=50,blank=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{}'.format(self.username)
+    
